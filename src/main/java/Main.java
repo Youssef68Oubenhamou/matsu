e21156afc545285bc40546701a6a8bcc31457bea
Youssef68Oubenhamou <youssefouben8@gmail.com>
2026-09-24 19:58:06 +0100

    arch: building the blueprint for the firt part of the framework which is the HTTP server!

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and Matsu!\n");
+
+        System.out.println("Let's get started hacking!\n");
+
+    }
+}
---
+++ src/test/java/MatsuTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MatsuTest {
+
+    @Test
+    void firstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
